96886471db4f64df450f52353e981a310e58e541
lisahuele <lisahuele@gmail.com>
2021-09-15 17:46:02 -0400

    password prompts

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,38 @@ function generatePassword() {
   var specialChar = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\'',']','^','_','`','{','|','}','~'];
   var numChar = ['0','1','2','3','4','5','6','7','8','9'];
 
+  //prompt pw length
+  var pwLength = parseInt(window.prompt("How long will your password be? Please choose a length between 8 and 128 characters"));
+
+  //validate pw length
+  while ( !(parseInt(pwLength) >=8 && parseInt(pwLength)<=128) ){
+    pwLength = parseInt(window.prompt("Your input was invalid! Please choose a length between 8 and 128 characters"))
+  };
+
+  //prompt pw types
+  while (i===0) {
+  
+    // lower case prompt
+    var lowercaseConfirm = window.prompt("Should the password contain lowercase characters? Type 'Y' for yes or 'N' for no").toUpperCase();
+    if (!lowercaseConfirm === 'Y' || !lowercaseConfirm === 'N') {
+      lowercaseConfirm = window.prompt("Invalid input. Should the password contain lowercase characters? Type 'Y' for yes or 'N' for no").toUpperCase();
+    } 
+    else if (lowercaseConfirm === 'Y') {
+      i++
+    } 
+  };
+
+  // upper case prompt
+  var uppercaseConfirm = window.prompt("Should the password contain uppercase characters? Type 'Y' for yes or 'N' for no").toUpperCase();
+  if (!uppercaseConfirm === 'Y' || !uppercaseConfirm === 'N') {
+    uppercaseConfirm = window.prompt("Invalid input. Should the password contain uppercase characters? Type 'Y' for yes or 'N' for no").toUpperCase();
+  } 
+  else if (uppercaseConfirm === 'Y') {
+    i++
+  } 
+};
+
+
 }
 
 // Write password to the #password input
