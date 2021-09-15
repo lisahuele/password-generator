58d6ade885b51f5179a1ce467243ddb298c0b645
lisahuele <lisahuele@gmail.com>
2021-09-15 17:35:19 -0400

    character array

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,17 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// generate password function
+function generatePassword() {
+  var generatePw = '';
+
+  // character array
+  var lowercaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var uppercaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var specialChar = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\'',']','^','_','`','{','|','}','~'];
+  var numChar = ['0','1','2','3','4','5','6','7','8','9'];
+
+}
 
 // Write password to the #password input
 function writePassword() {
